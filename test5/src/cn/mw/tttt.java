80e6a138d997eb3916cd2749ca92b39b12ac7c07
jeckma <395673181@qq.com>
2021-12-22 14:27:31 +0800

    add test5

---
+++ test5/src/cn/mw/tttt.java
@@ -0,0 +1,12 @@
+package cn.mw;
+
+public class tttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("11111222");
+		System.out.println("33333333333333");
+		system.out.println(55555);
+	}
+
+}
